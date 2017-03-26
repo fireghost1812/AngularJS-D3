3862183b7117d48efefe416c5d279f1f0eb8b535
fireghost1812 <fireghost1812@yandex.ru>
2017-03-26 20:43:30 +0300

    create border

--- assets/js/directives/kitEditor.js
+++ assets/js/directives/kitEditor.js
@@ -4,8 +4,68 @@ module.exports = ['d3Factory',function (d3Factory) {
     scope:true,
     restrict:'A',
     link:function($scope,$element,$attrs){
-      $scope.myProperty=41;
-      console.log($scope.myProperty);
+      d3Factory.then(function (d3) {
+        $scope.editor = {
+          grid:{
+            sizeXMm:5,
+            sizeYMm:5
+          },
+          position:{
+            x:0,
+            y:0
+          },
+          pageProperties:{
+            widthMm:297,
+            heightMm:210
+          },
+          svg:{},
+          features:{}
+        };
+
+        $scope.editor.svg.rootNode = d3.select($element[0]).append('svg')
+          .attr('id','svg-editor');
+
+        const conversionRect = $scope.editor.svg.rootNode.append('rect')
+          .attr('width','1mm')
+          .attr('height','1mm');
+
+        $scope.editor.features.pixelsPerMm = conversionRect.node().getBBox().width;
+        conversionRect.remove();
+
+        const g = $scope.editor.svg.rootNode.append('g')
+          .attr('transform','translate(0,0)');
+
+        $scope.editor.svg.underlay = g.append('rect')
+          .attr('class','underlay')
+          .attr('width','100%')
+          .attr('height','100%');
+
+        $scope.editor.svg.container = g.append('g')
+          .attr('class','svg-container')
+
+        const borderFrame = $scope.editor.svg.container.append('rect')
+          .attr('class','svg-border')
+          .attr('x','0')
+          .attr('y','0')
+          .attr('weight','0')
+          .attr('height','0');
+
+        const DURATION = 800;
+
+        const pageWidth = $scope.editor.pageProperties.widthMm*
+          $scope.editor.features.pixelsPerMm;
+        const pageHeight = $scope.editor.pageProperties.widthMm*
+          $scope.editor.features.pixelsPerMm;
+
+        borderFrame
+          .transition()
+          .duration(DURATION)
+          .attr('weight',pageWidth)
+          .attr('height',pageHeight);
+
+      }).catch(function (error) {
+        console.log(error);
+      });
     }
   }
 }];
--- assets/js/modules/d3.js
+++ assets/js/modules/d3.js
@@ -1,6 +1,26 @@
 angular.module('d3',[])
 
-  .factory('d3Factory',[function () {
-    return {};
-  }
-  ]);
+  .factory('d3Factory',['$document','$rootScope','$window','$q',
+    function ($document,$rootScope,$window,$q) {
+      const scriptTag = $document[0].createElement('script');
+
+      scriptTag.async = true;
+      scriptTag.type = 'text/javascript';
+      scriptTag.src = '../vendor/d3/d3.js';
+
+      scriptTag.onload = function () {
+        console.log('ok!')
+      };
+
+      $document[0].body.appendChild(scriptTag);
+      return $q(function(resolve,reject){
+        scriptTag.onload = function () {
+          $rootScope.$apply();
+          if($window.d3){
+            resolve($window.d3);
+          } else {
+            reject('Error!');
+          }
+        };
+      });
+  }]);
